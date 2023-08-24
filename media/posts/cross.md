1cec35f263b26429cbece2c32c602ff5366fb547
abaker42 <anthony.baker390@gmail.com>
2023-08-24 01:19:14 -0400

    dynamic data added

---
+++ media/posts/cross.md
@@ -0,0 +1,10 @@
+---
+title: A First Mardown Post0
+date: '2023/08/21'
+image: cross.jpg
+excerpt: This is a dummy excerpt about nothing
+isFeatured: true
+---
+# This is a title
+
+This is some regular text and a [link](https://google.com)
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,55 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
+import { getFeaturedPosts } from "@/lib/post-utils";
 import React, { Fragment } from "react";
 
-const DUMMY = [
-	{
-		title: "Know God",
-		image: "jesus-crux.jpg",
-		excerpt: "God is my healer. He is my redeemer. He is m...",
-		date: "2023-07-03",
-		slug: "kamla24",
-	},
-	{
-		title: "Trust God",
-		image: "holy-bible.jpg",
-		excerpt: "God is trust. He will never leave forsake me in my cold...",
-		date: "2023-03-25",
-		slug: "kakf3k-sknsg",
-	},
-	{
-		title: "Finding Your Way",
-		image: "cross.jpg",
-		excerpt: "God is seeking lost souls. He leaves his flock in sear...",
-		date: "2023-03-25",
-		slug: "kauru-r9rg",
-	},
-	{
-		title: "Studying the Bible",
-		image: "study-bible.jpg",
-		excerpt: "The holy spirit dwells in you when you are save. It is the...",
-		date: "2023-09-17",
-		slug: "kaujhjru-r9rjg",
-	},
-];
-function HomePage() {
+// const DUMMY = [
+// 	{
+// 		title: "Know God",
+// 		image: "jesus-crux.jpg",
+// 		excerpt: "God is my healer. He is my redeemer. He is m...",
+// 		date: "2023-07-03",
+// 		slug: "kamla24",
+// 	},
+// 	{
+// 		title: "Trust God",
+// 		image: "holy-bible.jpg",
+// 		excerpt: "God is trust. He will never leave forsake me in my cold...",
+// 		date: "2023-03-25",
+// 		slug: "kakf3k-sknsg",
+// 	},
+// 	{
+// 		title: "Finding Your Way",
+// 		image: "cross.jpg",
+// 		excerpt: "God is seeking lost souls. He leaves his flock in sear...",
+// 		date: "2023-03-25",
+// 		slug: "kauru-r9rg",
+// 	},
+// 	{
+// 		title: "Studying the Bible",
+// 		image: "study-bible.jpg",
+// 		excerpt: "The holy spirit dwells in you when you are save. It is the...",
+// 		date: "2023-09-17",
+// 		slug: "kaujhjru-r9rjg",
+// 	},
+// ];
+function HomePage(props) {
 	return (
 		<Fragment>
 			<Hero />
-			<FeaturedPosts posts={DUMMY} />
+			<FeaturedPosts posts={props.posts} />
 		</Fragment>
 	);
 }
 
+export function getStaticProps() {
+	const featuredPosts = getFeaturedPosts();
+
+	return {
+		props: {
+			posts: featuredPosts,
+		},
+		revalidate: 1800 //30min
+	};
+}
 export default HomePage;
